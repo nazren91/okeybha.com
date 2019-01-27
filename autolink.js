66fc92eafb6b2d4ed45e82a3cec10797e06c931e
NazreN <gadib2000@gmail.com>
2019-01-27 23:45:45 +0800

    Update autolink.js

--- autolink.js
+++ autolink.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.panduanblog.online/p/go_to_url.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.onet4u.com/p/go-url.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
